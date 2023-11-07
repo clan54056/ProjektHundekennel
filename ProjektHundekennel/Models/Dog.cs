98eea3cd79048624ff92c0ff93f60203d1ce6997
Helle <h@DESKTOP-N1RD298>
2023-11-07 14:49:28 +0100

    ailment enum started

---
+++ ProjektHundekennel/Models/Dog.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ProjektHundekennel.Models
+{
+    public class Dog
+    {
+        int dogId; //primary key
+        string pedigreeId;
+        string tato;
+        string name;
+        string breeder;
+        string father; //henfører til pedigreeId
+        string mother;
+        string dkkTitles;
+        string titles;
+        DateTime lastVisit;
+        int count;
+        DateTime birthDate;
+        char sex;
+        string colour;
+        bool dead;
+        string aK;
+        bool breedingStatus;
+        bool mentalDescribed;
+        Image image;
+        DateTime dateAdded;
+        int ailments;//skal henføre til ailmentId?
+
+        //public CreateDog(int dogId, string pedigreeId, string father, string mother, DateTime birthDate)
+        //{
+        //    this.dogId = dogId;
+        //    this.pedigreeId = pedigreeId;
+        //    this.father = father;
+        //    this.mother = mother;
+        //    this.birthDate = birthDate;
+        //}
+
+        //public void ViewDog()
+        //{
+
+        //}
+
+        //public void UpdateDog()
+        //{
+
+        //}
+
+        //public void DeleteDog()
+        //{
+
+        //}
+
+    }
+}
---
+++ ProjektHundekennel/Models/ailment_enums.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektHundekennel.Models
+{
+    enum ailment_sp
+    {
+        sp0 = 0,
+        sp1 = 1,
+        sp2 = 2,
+        sp3 = 3,
+        sp4 = 4
+    }
+    enum ailment_hz
+    {
+
+    }
+	enum ailment_hd
+	{
+        hd0 = 0,
+        A = 1,
+        A2 = 2,
+        B = 3,
+        B1 = 4,
+        B2 = 5,
+
+	}
+	enum ailment_ad
+	{
+
+	}
+}
