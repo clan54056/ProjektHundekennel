09d7303ca53a27620d2c554c9f601fcbbcb6841b
clan54056 <128791046+clan54056@users.noreply.github.com>
2023-11-09 11:33:00 +0100

    ailments

---
+++ ProjektHundekennel/Models/ailments_enums.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektHundekennel.Models
+{
+    enum ailment_sp
+    {
+        _0 = 0,
+        _1 = 1,
+        _2 = 2,
+        _3 = 3,
+        _4 = 4
+    }
+    enum ailment_hz
+    {
+
+    }
+    enum ailment_hd
+    {
+        _0 = 0,
+        A = 1,
+        A2 = 2,
+        B = 3,
+        B1 = 4,
+        B2 = 5,
+        B3 = 6,
+        C = 7,
+        D = 8,
+
+    }
+    enum ailment_ad
+    {
+
+    }
+}
