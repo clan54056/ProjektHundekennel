00d711f80f52eb3793d83d3503a3a6b8897a15c3
Helle <h@DESKTOP-N1RD298>
2023-11-09 11:03:12 +0100

    ny

--- ProjektHundekennel/Models/Dog.cs
+++ ProjektHundekennel/Models/Dog.cs
@@ -10,35 +10,39 @@ namespace ProjektHundekennel.Models
     public class Dog
     {
         int dogId; //primary key
-        string pedigreeId;
-        string tato;
-        string name;
-        string breeder;
-        string father; //henfører til pedigreeId
-        string mother;
-        string dkkTitles;
-        string titles;
-        DateTime lastVisit;
-        int count;
-        DateTime birthDate;
-        char sex;
-        string colour;
-        bool dead;
-        string aK;
-        bool breedingStatus;
-        bool mentalDescribed;
-        Image image;
-        DateTime dateAdded;
-        int ailments;//skal henføre til ailmentId?
 
-        //public CreateDog(int dogId, string pedigreeId, string father, string mother, DateTime birthDate)
-        //{
-        //    this.dogId = dogId;
-        //    this.pedigreeId = pedigreeId;
-        //    this.father = father;
-        //    this.mother = mother;
-        //    this.birthDate = birthDate;
-        //}
+        string pedigreeId { get; set; }
+        string tato { get; set; }
+		string name { get; set; }
+		string breeder { get; set; }
+
+		string father; //henfører til pedigreeId
+        string mother;
+
+        string dkkTitles { get; set; }
+		string titles { get; set; }
+		
+		int count { get; set; }
+		DateTime birthDate { get; set; }
+		char sex { get; set; }
+		string colour { get; set; }
+		bool dead { get; set; }
+		string aK { get; set; }
+		bool breedingStatus { get; set; }
+		bool mentalDescribed { get; set; }
+		Image image;
+        
+
+		int ailments;//skal henføre til ailmentId?
+
+        public CreateDog(int dogId, string pedigreeId, string name, string father, string mother, DateTime birthDate)
+        {
+            this.dogId = dogId;
+            this.pedigreeId = pedigreeId;
+            this.father = father;
+            this.mother = mother;
+            this.birthDate = birthDate;
+        }
 
         //public void ViewDog()
         //{
--- ProjektHundekennel/Models/ailment_enums.cs
+++ ProjektHundekennel/Models/ailment_enums.cs
@@ -8,11 +8,11 @@ namespace ProjektHundekennel.Models
 {
     enum ailment_sp
     {
-        sp0 = 0,
-        sp1 = 1,
-        sp2 = 2,
-        sp3 = 3,
-        sp4 = 4
+        _0 = 0,
+        _1 = 1,
+        _2 = 2, 
+        _3 = 3, 
+        _4 = 4
     }
     enum ailment_hz
     {
@@ -20,12 +20,15 @@ namespace ProjektHundekennel.Models
     }
 	enum ailment_hd
 	{
-        hd0 = 0,
+        _0 = 0,
         A = 1,
         A2 = 2,
         B = 3,
         B1 = 4,
         B2 = 5,
+        B3 = 6,
+        C = 7,
+        D = 8,
 
 	}
 	enum ailment_ad
