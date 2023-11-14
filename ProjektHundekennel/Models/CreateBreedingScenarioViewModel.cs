9d1d00584ce698c081fff88d2a7e47f3e3e29b94
Nikoline Brink Kjærsgaard <nbkj54129@edu.ucl.dk>
2023-11-14 21:55:24 +0100

    CreateBreedingprogress

---
+++ ProjektHundekennel/Models/CreateBreedingScenarioViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektHundekennel.Models
+{
+    public class CreateBreedingScenarioViewModel
+    {
+      public string pedigree_id1 { get; set; }
+      public string dog_name1 { get; set; }
+      public string colour1 { get; set; }
+    public string mother_id1 { get; set; }
+    public string mother_name1 { get; set; }
+    public string mother_colour { get; set; }
+        public string father_id1 { get; set; }
+    public string father_name1 { get; set; }
+    public string father_colour { get; set; }
+    public string grandmother_m_id1 { get; set; }
+    public string grandmother_m_name1 { get; set; }
+    public string grandmother_m_colour { get; set; }
+    public string grandfather_m_id1 { get; set; }
+    public string grandfather_m_name1 { get; set; }
+    public string grandfather_m_colour { get; set; }
+    
+    }
+}
